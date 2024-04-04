257793072b6bad738a130903795785c9177c03d8
mike6321 <mike6321@naver.com>
2024-04-04 13:54:08 +0900

    chore : setting ignore

---
+++ coupon-core/src/main/java/com/example/couponcore/CouponCoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.couponcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponCoreApplication {
+
+	public static void main(String[] args) {
+		System.out.println();
+		SpringApplication.run(CouponCoreApplication.class, args);
+	}
+
+}
