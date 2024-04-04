5113664615e68859291ac525843d99f0ffdd1f2c
mike6321 <mike6321@naver.com>
2024-04-04 15:34:36 +0900

    feat : project setting

--- coupon-api/build.gradle.kts
+++ coupon-api/build.gradle.kts
@@ -1,21 +1,6 @@
-plugins {
-	java
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
-}
-
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-
-java {
-	sourceCompatibility = JavaVersion.VERSION_17
-}
-
-repositories {
-	mavenCentral()
-}
-
 dependencies {
+	implementation(project(":coupon-core"))
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
