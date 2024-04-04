bb9e0ee5a063c1be5ef03fa27af3e28515eb7b53
mike6321 <mike6321@naver.com>
2024-04-04 16:13:46 +0900

    feat : project setting

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ bootJar.enabled = false
 
 plugins {
 	java
-	id("org.springframework.boot") version "3.2.4"
-	id("io.spring.dependency-management") version "1.1.4"
+	id("org.springframework.boot") version "3.1.5"
+	id("io.spring.dependency-management") version "1.1.3"
 }
 
 group = "com.example"
---
+++ coupon-core/src/main/java/com/example/couponcore/CouponCoreConfiguration.java
@@ -0,0 +1,10 @@
+package com.example.couponcore;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableAutoConfiguration
+public class CouponCoreConfiguration {
+
+}
